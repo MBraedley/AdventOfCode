3f78e2d705844e2d8b09cc2aadc33bc3a1d989a1
Michael Braedley <mike.braedley@gmail.com>
2018-12-22 08:40:24 -0400

    Finish Day 2

--- AdventOfCode2/AdventOfCode2/AdventOfCode2.cpp
+++ AdventOfCode2/AdventOfCode2/AdventOfCode2.cpp
@@ -4,8 +4,80 @@
 #include "pch.h"
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <set>
+
+using namespace std;
+
+bool oneCharOff(string a, string b, int& diffPos)
+{
+	if (a.size() != b.size())
+		return false;
+
+	int diffCount = 0;
+	for (int i = 0; i < a.size(); i++)
+	{
+		if (a[i] != b[i])
+		{
+			diffCount++;
+			diffPos = i;
+		}
+
+		if (diffCount >= 2)
+			return false;
+	}
+
+	return diffCount == 1;
+}
 
 int main()
 {
+	string filename = "C:\\Dev\\AdventOfCode\\AdventOfCode2\\input2.txt";
+	//int twosCount = 0;
+	//int threesCount = 0;
 
+	set<string> candidates;
+
+	ifstream fin(filename);
+	string boxName;
+	while (getline(fin, boxName))
+	{
+		multiset<char> s;
+		for (char c : boxName)
+		{
+			s.insert(c);
+		}
+		bool hasDouble = false;
+		bool hasTriple = false;
+
+		for (auto it : s)
+		{
+			if (s.count(it) == 2)
+				hasDouble = true;
+			if (s.count(it) == 3)
+				hasTriple = true;
+		}
+
+		//if (hasDouble)
+		//	twosCount++;
+		//if (hasTriple)
+		//	threesCount++;
+
+		if (hasDouble || hasTriple)
+		{
+			for (auto str : candidates)
+			{
+				int pos;
+				if (oneCharOff(boxName, str, pos))
+				{
+					cout << boxName << endl << str << endl;
+					cout << boxName.substr(0, pos) << " " << boxName.substr(pos + 1) << endl;
+					return 0;
+				}
+			}
+			candidates.insert(boxName);
+		}
+	}
+
+//	cout << twosCount * threesCount;
 }
